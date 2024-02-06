0e2f27d72e0b1f231d8b37d5a418b96f45c90c0b
Ankit Kumar <kanaujiya2ankit@gmail.com>
2024-02-06 22:47:24 +0530

    cors

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const corsOptions = {
   credentials: true,
 };
 
-// app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "https://chatt-ap.netlify.app");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  if (req.method === "OPTIONS") {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+app.use(cors(corsOptions));
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "https://chatt-ap.netlify.app");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   if (req.method === "OPTIONS") {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
